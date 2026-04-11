62c5e005c866a676200f0bae6f10bd5c6bab4c82
max <maxim22ananin@gmail.com>
2026-04-11 09:55:47 +0400

    fix conflict

--- home/home.nix
+++ home/home.nix
@@ -8,8 +8,6 @@
   home.username = "max";
   home.homeDirectory = "/home/max";
 
-  nixpkgs.config.allowUnfree = true;
-
   home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
--- modules/packages.nix
+++ modules/packages.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+
   programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
