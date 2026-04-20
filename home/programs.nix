3f46334d97fe281ad06721440a3e7f2cb03fdead
max <maxim22ananin@gmail.com>
2026-04-20 12:46:24 +0400

    Install zsh and refactor configs

--- home/programs.nix
+++ home/programs.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./aliases.nix
+  ];
+
   programs.git = {
     enable = true;
     settings = {
@@ -11,16 +15,35 @@
 
   programs.bash = {
     enable = true;
-    shellAliases = {
-      ll = "ls -la";
-      k = "kubectl";
 
-      # NixOS aliases
-      nixos-rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-      nixos-update = "sudo nixos-rebuild switch --upgrade --flake /etc/nixos#nixos";
-      nixos-clean = "sudo nix-collect-garbage -d";
-      nixos-optimise = "sudo nix-store --optimise";
-      nixos-diff = "nix store diff-closures /run/current-system /run/booted-system";
+    # shellAliases = {
+    #   ll = "ls -la";
+    #   k = "kubectl";
+
+    #   # NixOS aliases
+    #   nixos-rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
+    #   nixos-update = "sudo nixos-rebuild switch --upgrade --flake /etc/nixos#nixos";
+    #   nixos-clean = "sudo nix-collect-garbage -d";
+    #   nixos-optimise = "sudo nix-store --optimise";
+    #   nixos-diff = "nix store diff-closures /run/current-system /run/booted-system";
+    # };
+  };
+
+  programs.zsh = {
+    enable = true;
+    
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "kubectl" "docker" "nix" ];
+      theme = "robbyrussell";
+    };
+    
+    shellAliases = {
+      gs = "git status";
+      gd = "git diff";
     };
   };
 }
