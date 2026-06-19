0ab10e73b865b3299e70221874eac7be0a5ffa3e
max <maxim22ananin@gmail.com>
2026-06-19 17:15:36 +0400

    Add how-to comment

--- home/aliases.nix
+++ home/aliases.nix
@@ -12,5 +12,11 @@
     nixos-clean = "sudo nix-collect-garbage -d";
     nixos-optimise = "sudo nix-store --optimise";
     nixos-diff = "nix store diff-closures /run/current-system /run/booted-system";
+
+    # To upgrade to a new NixOS release:
+    # 1. Edit /etc/nixos/flake.nix: bump nixpkgs.url and home-manager.url
+    #    (e.g. nixos-25.11 -> nixos-26.05, release-25.11 -> release-26.05)
+    # 2. cd /etc/nixos && sudo nix flake update
+    # 3. Run nixos-rebuild (alias above) to apply
   };
 }
