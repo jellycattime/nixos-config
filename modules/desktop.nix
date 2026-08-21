d1e99bc317e82dfc9bb0100c10f422df4510face
max <maximananin22@gmail.com>
2026-08-21 19:32:48 +0400

    Fix for displayManager

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,14 +1,21 @@
 # modules/desktop.nix
 { config, pkgs, ... }:
-
 {
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = false;
+  };
+  services.desktopManager.plasma6.enable = true;
   services.xserver.enable = true;
 
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  hardware.enableRedistributableFirmware = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
 
   services.libinput.enable = true;
-
   services.pipewire = {
     enable = true;
     pulse.enable = true;
--- modules/kde-themes.nix
+++ modules/kde-themes.nix
@@ -4,11 +4,11 @@
 {
   environment.systemPackages = with pkgs; [
     kdePackages.kalk
-    arc-kde-theme
-    adapta-kde-theme
-    twilight-kde
-    qogir-kde
-    sweet-nova
+#    arc-kde-theme
+#    adapta-kde-theme
+#    twilight-kde
+#    qogir-kde
+#    sweet-nova
     kdePackages.breeze-plymouth
     kdePackages.plasma-integration
     kdePackages.plasma-workspace-wallpapers
