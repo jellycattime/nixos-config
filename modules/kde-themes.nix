d928b90d4534a900fbaa0090b763044e6a5a0b6d
max <maximananin22@gmail.com>
2026-08-23 18:32:20 +0400

    Add themes for plasma

--- modules/kde-themes.nix
+++ modules/kde-themes.nix
@@ -1,16 +1,36 @@
 # modules/kde-themes.nix
-{ config, pkgs, ... }:
-
+{ config, pkgs, nixpkgs-unstable, ... }:
+let
+  unstable = import nixpkgs-unstable {
+    system = pkgs.stdenv.hostPlatform.system;
+    config.allowUnfree = true;
+  };
+in
 {
   environment.systemPackages = with pkgs; [
     kdePackages.kalk
-#    arc-kde-theme
-#    adapta-kde-theme
-#    twilight-kde
-#    qogir-kde
-#    sweet-nova
+    arc-kde-theme
+    adapta-kde-theme
+    aritim-dark
+    twilight-kde
+    qogir-kde
+    pitch-black
+    sweet-nova
+    catppuccin-kde
+    colorfuldarkglobal6-kde
+    whitesur-kde
+    utterly-nord-plasma
+    utterly-round-plasma-style
+    nordic
     kdePackages.breeze-plymouth
     kdePackages.plasma-integration
     kdePackages.plasma-workspace-wallpapers
+    klassy
+    # Kvantum
+    kdePackages.qtstyleplugin-kvantum
+    catppuccin-kvantum
+    gruvbox-kvantum
+    unstable.materia-everforest-kvantum
+
   ];
 }
